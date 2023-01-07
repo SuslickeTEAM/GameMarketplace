1a2b40bc102182d007cc8d162a6706e891af1b83
Madara <tolkina27@gmail.com>
2023-01-07 13:12:17 -0800

    создала модели

--- GameMarketplace/Market/models.py
+++ GameMarketplace/Market/models.py
@@ -1,4 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Basket(models.Model):
+    basket_product = models.ForeignKey('Basket_product', on_delete=models.CASCADE, related_name='+')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits = 30, decimal_places = 5)
+    rate = models.ForeignKey('Rating', on_delete=models.CASCADE, related_name='+')
+    img = models.ImageField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+class Basket_product(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    basket = models.OneToOneField(Basket,on_delete=models.CASCADE,related_name='+')
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+class Product_info(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    rate = models.PositiveIntegerField()
