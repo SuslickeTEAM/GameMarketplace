603640092579e5e294b6ba6827c5c25b0661dd58
Suslicke <suslicketeam@gmail.com>
2023-01-08 01:16:40 +0400

    some changes

--- GameMarketplace/Market/models.py
+++ GameMarketplace/Market/models.py
@@ -8,7 +8,7 @@ class Basket(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
-    price = models.DecimalField(max_digits = 30, decimal_places = 5)
+    price = models.DecimalField(max_digits=30, decimal_places=5)
     rate = models.ForeignKey('Rating', on_delete=models.CASCADE, related_name='+')
     img = models.ImageField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
@@ -16,17 +16,20 @@ class Product(models.Model):
 
 class Basket_product(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE)
-    basket = models.OneToOneField(Basket,on_delete=models.CASCADE,related_name='+')
+    basket = models.OneToOneField(Basket, on_delete=models.CASCADE, related_name='+')
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
 
+
 class Product_info(models.Model):
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=128)
 
+
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     rate = models.PositiveIntegerField()
