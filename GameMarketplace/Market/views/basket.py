ef7878f13fa9a58601db54c2089e547d699f6856
Lag <vitya-dolzhenkov@mail.ru>
2023-01-11 19:00:14 +0300

    готовая хайповая корзина тети зины

--- GameMarketplace/Market/views/basket.py
+++ GameMarketplace/Market/views/basket.py
@@ -5,7 +5,10 @@ from ..models import *
 def view_basket(request):
     basket = Basket.objects.get(user=request.user)
     basket_items = Basket_product.objects.filter(basket=basket)
-    return render(request,  'Basket/basket.html', {"basket_items":basket_items})
+    basket_total = 0
+    for a in basket_items:
+        basket_total += int(a.quantity) * int(a.product.price)
+    return render(request,  'Basket/basket.html', {"basket_items":basket_items, "basket_total":basket_total, "basket":basket})
 
 def add_basket(request, pk):
     if Basket.objects.filter(user=request.user).exists():
@@ -20,4 +23,17 @@ def add_basket(request, pk):
         basket = Basket.objects.create(user=request.user)
         product =  get_object_or_404(Product, pk=pk)
         basket_items = Basket_product.objects.create(product=product, basket=basket, quantity=1)
+    return redirect("view_basket")
+
+def addition_basket(request, product, basket):
+    basket_product = Basket_product.objects.get(pk=product)
+    basket_items = Basket_product.objects.filter(pk=product, basket_id=basket).update(quantity = int(basket_product.quantity) + 1)
+    return redirect("view_basket")
+
+def subtraction_basket(request, product, basket):
+    basket_product = Basket_product.objects.get(pk=product)
+    if basket_product.quantity == 1:
+        basket_product.delete()
+    else:
+        basket_items = Basket_product.objects.filter(pk=product, basket_id=basket).update(quantity = int(basket_product.quantity) - 1)
     return redirect("view_basket")
