1b6f5097ad66dcbe9bd3b7e329ede09d3301284d
Lag <vitya-dolzhenkov@mail.ru>
2023-01-12 18:10:15 +0300

    Не работает фильтр

--- GameMarketplace/Market/models.py
+++ GameMarketplace/Market/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name = ("Пользователь"))
     created_at = models.DateTimeField(auto_now_add=True)
@@ -40,7 +39,7 @@ class Basket_product(models.Model):
    
 class Category(models.Model):
     name = models.CharField(max_length=128, verbose_name = ("Наименование категории"))
-    
+
     def __str__(self):
         return f'{self.name}'
     
